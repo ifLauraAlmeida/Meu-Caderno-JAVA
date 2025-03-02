49204b41634e04412cf963e706af67ae34c2fd09
Laura Almeida <oddlauraink@hotmail.com>
2025-03-02 18:24:00 -0300

    visualizando o lambda

---
+++ funcoes/Lambda.java
@@ -0,0 +1,14 @@
+package funcoes;
+
+import java.util.function.Consumer;
+
+public class Lambda {
+    
+    public static void main (String[] args) {
+        //Exemplo de função lambda que implementa a interface funcional Consumer
+        Consumer<String> imprimirMensagem = mensagem -> System.out.println(mensagem);
+
+        //Usando a função lambda para imprimir uma mensagem
+        imprimirMensagem.accept("Olá, mundo! Este é um exemplo da função Lambda");
+    }
+}
