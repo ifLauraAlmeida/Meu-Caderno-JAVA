71c7294880f160495b98bdbeb449af88bbadde16
Laura Almeida <oddlauraink@hotmail.com>
2025-03-04 10:27:57 -0300

    busca sequencial

--- MecanismosDeBusca/BuscaSequencialExemplo.java
+++ MecanismosDeBusca/BuscaSequencialExemplo.java
@@ -1,14 +1,6 @@
 package MecanismosDeBusca;
 
 public class BuscaSequencialExemplo {
-    public static int buscaSequencial(int[] arr, int valor) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == valor) {
-                return i; // Retorna a posição do número encontrado
-            }
-        }
-        return -1; // Retorna -1 se o número não estiver na lista
-    }
     public static void main(String[] args) {
         int[] numeros = {3, 7, 2, 9, 5}; // Nossa lista de números
         int procurado = 9; // Número que queremos encontrar
@@ -22,5 +14,12 @@ public class BuscaSequencialExemplo {
         }
     }
 
-   
+    public static int buscaSequencial(int[] arr, int valor) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == valor) {
+                return i; // Retorna a posição do número encontrado
+            }
+        }
+        return -1; // Retorna -1 se o número não estiver na lista
+    }
 }
